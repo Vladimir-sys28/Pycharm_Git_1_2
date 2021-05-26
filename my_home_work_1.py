df3c76f020c06f2edb8cf38582c3a43e2edbc61d
Vladigors <biutycool@bk.ru>
2021-05-26 20:38:58 +0300

    First Create commit 1

--- my_home_work_1.py
+++ my_home_work_1.py
@@ -1,7 +1,8 @@
-my_word = 'working'
+my_word = 'work'
 s = len(my_word)
 if (s % 2) == 0:
     string = (my_word[int(len(my_word / 2) - 1)] + (my_word[int(len(my_word / 2))]))
     print(s)
 else:
     print((my_word[int(len(my_word)/2)]))
+# task not work
