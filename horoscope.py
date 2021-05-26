5dc274af30150f033f0ee243950edf048cbca39a
Vladigors <biutycool@bk.ru>
2021-05-26 22:29:39 +0300

    I download working code horoscope from python project

---
+++ horoscope.py
@@ -0,0 +1,197 @@
+from __future__ import print_function
+my_day = input('Enter day:')
+my_day = int(my_day)
+my_month = input('Enter month:')
+my_list_ar = (my_day, my_month)
+nums_a_m = [x for x in range(19, 32)]
+my_m = ['Mar']
+my_ma = (my_m * 13)
+aries_m = list(zip(nums_a_m, my_ma))
+item = my_list_ar
+if item in aries_m:
+    print(" Aries")
+else:
+    nums_a_m = [x for x in range(1, 21)]
+my_m = ['Apr']
+my_ma = (my_m * 13)
+aries_m = list(zip(nums_a_m, my_ma))
+item = my_list_ar
+if (item in aries_m):
+    print(" Aries")
+else:
+    nums_a_m = [x for x in range(21, 31)]
+my_m = ['Apr']
+my_ma = (my_m * 10)
+aries_m = list(zip(nums_a_m, my_ma))
+item = my_list_ar
+if (item in aries_m):
+    print(" Taurus")
+else:
+    nums_a_m = [x for x in range(1, 22)]
+    my_m = ['May']
+    my_ma = (my_m * 21)
+    aries_m = list(zip(nums_a_m, my_ma))
+    item = my_list_ar
+    if (item in aries_m):
+        print(" Taurus")
+    else:
+        nums_a_m = [x for x in range(22, 32)]
+    my_m = ['May']
+    my_ma = (my_m * 10)
+    aries_m = list(zip(nums_a_m, my_ma))
+    item = my_list_ar
+    if (item in aries_m):
+        print(" Gemini")
+    else:
+        nums_a_m = [x for x in range(1, 22)]
+    my_m = ['Jun']
+    my_ma = (my_m * 21)
+    aries_m = list(zip(nums_a_m, my_ma))
+    item = my_list_ar
+    if (item in aries_m):
+        print(" Gemini")
+    else:
+        nums_a_m = [x for x in range(22, 31)]
+    my_m = ['Jun']
+    my_ma = (my_m * 9)
+    aries_m = list(zip(nums_a_m, my_ma))
+    item = my_list_ar
+    if (item in aries_m):
+        print(" Cancer")
+    else:
+        nums_a_m = [x for x in range(1, 23)]
+    my_m = ['Jul']
+    my_ma = (my_m * 22)
+    aries_m = list(zip(nums_a_m, my_ma))
+    item = my_list_ar
+    if (item in aries_m):
+        print(" Cancer")
+    else:
+        nums_a_m = [x for x in range(23, 32)]
+    my_m = ['Jul']
+    my_ma = (my_m * 9)
+    aries_m = list(zip(nums_a_m, my_ma))
+    item = my_list_ar
+    if (item in aries_m):
+        print(" Leo")
+    else:
+        nums_a_m = [x for x in range(1, 23)]
+    my_m = ['Aug']
+    my_ma = (my_m * 22)
+    aries_m = list(zip(nums_a_m, my_ma))
+    item = my_list_ar
+    if (item in aries_m):
+        print(" Leo")
+    else:
+        nums_a_m = [x for x in range(23, 32)]
+        my_m = ['Aug']
+        my_ma = (my_m * 9)
+        aries_m = list(zip(nums_a_m, my_ma))
+        item = my_list_ar
+        if (item in aries_m):
+            print(" Virgo")
+        else:
+            nums_a_m = [x for x in range(1, 23)]
+        my_m = ['Sep']
+        my_ma = (my_m * 22)
+        aries_m = list(zip(nums_a_m, my_ma))
+        item = my_list_ar
+        if (item in aries_m):
+            print(" Virgo")
+        else:
+            nums_a_m = [x for x in range(23, 31)]
+        my_m = ['Sep']
+        my_ma = (my_m * 9)
+        aries_m = list(zip(nums_a_m, my_ma))
+        item = my_list_ar
+        if (item in aries_m):
+            print(" Libra")
+        else:
+            nums_a_m = [x for x in range(1, 23)]
+        my_m = ['Oct']
+        my_ma = (my_m * 22)
+        aries_m = list(zip(nums_a_m, my_ma))
+        item = my_list_ar
+        if (item in aries_m):
+            print(" Libra")
+        else:
+            nums_a_m = [x for x in range(23, 32)]
+        my_m = ['Oct']
+        my_ma = (my_m * 9)
+        aries_m = list(zip(nums_a_m, my_ma))
+        item = my_list_ar
+        if (item in aries_m):
+            print(" Scorpio")
+        else:
+            nums_a_m = [x for x in range(1, 23)]
+        my_m = ['Nov']
+        my_ma = (my_m * 22)
+        aries_m = list(zip(nums_a_m, my_ma))
+        item = my_list_ar
+        if (item in aries_m):
+            print(" Scorpio")
+        else:
+            nums_a_m = [x for x in range(23, 31)]
+        my_m = ['Nov']
+        my_ma = (my_m * 8)
+        aries_m = list(zip(nums_a_m, my_ma))
+        item = my_list_ar
+        if (item in aries_m):
+            print(" Sagittarius")
+        else:
+            nums_a_m = [x for x in range(1, 22)]
+        my_m = ['Dec']
+        my_ma = (my_m * 21)
+        aries_m = list(zip(nums_a_m, my_ma))
+        item = my_list_ar
+        if (item in aries_m):
+            print(" Sagittarius")
+        else:
+            nums_a_m = [x for x in range(22, 32)]
+        my_m = ['Dec']
+        my_ma = (my_m * 10)
+        aries_m = list(zip(nums_a_m, my_ma))
+        item = my_list_ar
+        if (item in aries_m):
+            print(" Capricorn")
+        else:
+            nums_a_m = [x for x in range(1, 21)]
+        my_m = ['Jan']
+        my_ma = (my_m * 21)
+        aries_m = list(zip(nums_a_m, my_ma))
+        item = my_list_ar
+        if (item in aries_m):
+            print(" Capricorn")
+        else:
+            nums_a_m = [x for x in range(21, 32)]
+        my_m = ['Jan']
+        my_ma = (my_m * 11)
+        aries_m = list(zip(nums_a_m, my_ma))
+        item = my_list_ar
+        if (item in aries_m):
+            print(" Aquarius")
+        else:
+            nums_a_m = [x for x in range(1, 20)]
+            my_m = ['Feb']
+            my_ma = (my_m * 19)
+            aries_m = list(zip(nums_a_m, my_ma))
+            item = my_list_ar
+            if (item in aries_m):
+                print(" Aquarius")
+            else:
+                nums_a_m = [x for x in range(20, 30)]
+            my_m = ['Feb']
+            my_ma = (my_m * 10)
+            aries_m = list(zip(nums_a_m, my_ma))
+            item = my_list_ar
+            if (item in aries_m):
+                print(" Pisces")
+            else:
+                nums_a_m = [x for x in range(1, 21)]
+            my_m = ['Mar']
+            my_ma = (my_m * 10)
+            aries_m = list(zip(nums_a_m, my_ma))
+            item = my_list_ar
+            if (item in aries_m):
+                print(" Pisces")
+
